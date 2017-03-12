8b18037b174b0dccfd7cce7b2bd8a3e40604f1fc
Dennis Ferron <system.windows.codeslinger@gmail.com>
2017-03-12 14:31:04 -0500

    Added Bullet physics hello world code.

--- main.cpp
+++ main.cpp
@@ -35,6 +35,11 @@ using namespace video;
 using namespace io;
 using namespace gui;
 
+#include "btBulletDynamicsCommon.h"
+#include "BulletCollision/Gimpact/btGImpactCollisionAlgorithm.h"
+
+#include <iostream>
+
 /*
 This is the main method. We can use void main() on every platform.
 On Windows platforms, we could also use the WinMain method
@@ -44,6 +49,36 @@ we use main().
 */
 int main(int argc, char** argv)
 {
+    btBroadphaseInterface* broadphase = new btDbvtBroadphase();
+    btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+    btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+    btGImpactCollisionAlgorithm::registerAlgorithm(dispatcher);
+    btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+    btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+    dynamicsWorld->setGravity(btVector3(0, -10, 0));
+
+    btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
+    btCollisionShape* fallShape = new btSphereShape(1);
+
+    btDefaultMotionState* groundMotionState =
+                new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
+
+    btRigidBody::btRigidBodyConstructionInfo
+                    groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
+            btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
+
+    dynamicsWorld->addRigidBody(groundRigidBody);
+
+    btDefaultMotionState* fallMotionState =
+                new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
+
+    btScalar mass = 1;
+        btVector3 fallInertia(0, 0, 0);
+        fallShape->calculateLocalInertia(mass, fallInertia);
+
+    btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
+        btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
+        dynamicsWorld->addRigidBody(fallRigidBody);
     /*
     The most important function of the engine is the 'createDevice'
     function. The Irrlicht Device can be created with it, which is the
@@ -128,6 +163,8 @@ int main(int argc, char** argv)
     */
     smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
 
+    int num_frames = 0;
+
     /*
     Ok, now we have set up the scene, lets draw everything:
     We run the device in a while() loop, until the device does not
@@ -153,6 +190,15 @@ int main(int argc, char** argv)
 
         driver->beginScene(clearFlag, clearColor, clearDepth, clearStencil, videoData, sourceRect);
 
+        dynamicsWorld->stepSimulation(1 / 60.f, 10);
+
+        if ((num_frames <= 240) && (num_frames % 60 == 0))
+        {
+            btTransform trans;
+            fallRigidBody->getMotionState()->getWorldTransform(trans);
+            std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
+        }
+
         smgr->drawAll();
         guienv->drawAll();
 
@@ -170,6 +216,27 @@ int main(int argc, char** argv)
     for more information.
     */
     device->drop();
+    device->drop();
+    device->drop();
+
+    dynamicsWorld->removeRigidBody(fallRigidBody);
+    delete fallRigidBody->getMotionState();
+    delete fallRigidBody;
+
+    dynamicsWorld->removeRigidBody(groundRigidBody);
+    delete groundRigidBody->getMotionState();
+    delete groundRigidBody;
+
+
+    delete fallShape;
+
+    delete groundShape;
+
+    delete dynamicsWorld;
+    delete solver;
+    delete dispatcher;
+    delete collisionConfiguration;
+    delete broadphase;
 
     return 0;
 }
