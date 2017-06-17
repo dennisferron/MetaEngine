374c064a71d175d196d8bad65287f3f647ad96e4
Dennis Ferron <system.windows.codeslinger@gmail.com>
2017-06-16 23:28:20 -0500

    Converted some headers from Io to C++.

--- Glue/Include/Glue/Model/Link.hpp
+++ Glue/Include/Glue/Model/Link.hpp
@@ -9,6 +9,8 @@ namespace Glue {
 class Graph;
 class LinkStyle;
 class Node;
+class Interaction;
+class Attribute;
 
 class Link
 {
@@ -22,59 +24,19 @@ private:
 public:
 
     // This list is shared among all Link clones
-    static std::vector<Interaction*> possibleInteractions := list()
+    static std::vector<Interaction*> possibleInteractions;
 
-    registerInteraction := method(interaction,
-        possibleInteractions append(interaction)
-    )
+    static void registerInteraction(Interaction*);
 
-    init := method(
-        setDomain(module Domain clone setSite(self))
-    )
+    Link(LinkStyle* newStyle);
 
-    with := method(newStyle,
-        setStyle(newStyle)
-    )
+    Link& setStyle(LinkStyle* value, Interaction* expectedInteraction);
+    Link& setFromNode(Node* value);
+    Link& setToNode(Node* value);
+    Link& setGraph(Graph* value);
+    Link& addAttribute(Attribute* attr, Interaction* expectedInteraction);
 
-    setStyle := method(value, expectedInteraction,
-        updateSlot("style", value)
-        domain addObject("style", style, expectedInteraction)
-        self
-    )
-
-    setFromNode := method(value,
-        updateSlot("fromNode", value)
-        // TODO:  Notify and remove old node value from domain.
-        if(fromNode != nil,
-            domain merge("fromNode", fromNode domain, "node")
-        )
-        self
-    )
-
-    setToNode := method(value,
-        updateSlot("toNode", value)
-        // TODO:  Notify and remove old node value from domain.
-        if(toNode != nil,
-            domain merge("toNode", toNode domain, "node")
-        )
-        self
-    )
-
-    setGraph := method(value,
-        updateSlot("graph", value)
-        // TODO:  Notify and remove old components from the domain.
-        domain merge("component", graph domain, "component")
-        self
-    )
-
-    addAttribute := method(attr, expectedInteraction,
-        domain addObject("link", attr, expectedInteraction)
-        self
-    )
-
-    findAttribute := method(attr,
-        domain findObject("link", attr)
-    )
+    Attribute* findAttribute(Attribute* attr);
 };
 
 }
