ab6a5bbba48d49a4f18d37b6451e66cd7725b4b3
Dennis Ferron <system.windows.codeslinger@gmail.com>
2017-08-13 23:27:15 -0500

    Working on GameObjStyle cpp"

--- Glue/Include/Glue/Styles/GameObjStyle.hpp
+++ Glue/Include/Glue/Styles/GameObjStyle.hpp
@@ -89,83 +89,79 @@ namespace Glue {
 
     public:
 
+        // Display shape
+        ObjShapes dispShape;
+
+        // Physical shape
+        ObjShapes physShape;
+
+        GameObjTypes gameObjType;
+
+        // Set physical and display shape at once.
+        GameObjStyle& setShape(ObjShapes s);
+
         // Location
-        Scalar x = 0;
-        Scalar y = 0;
-        Scalar z = 0;
+        Scalar x;
+        Scalar y;
+        Scalar z;
 
         // Set x,y,z all at once
-        GameObjStyle& setPos(Scalar x, Scalar y, Scalar z);
-
-        ObjPos getPos() const;
+        GameObjStyle& setPos(Scalar x, Scalar y, Scalar z)
+        std::tuple<Scalar, Scalar, Scalar> getPos() const;
 
         // Velocity
-        Scalar xv = 0;
-        Scalar yv = 0;
-        Scalar zv = 0;
+        Scalar xv;
+        Scalar yv;
+        Scalar zv;
 
         // Set xv, yv, zv all at once
         GameObjStyle& setVel(Scalar xv, Scalar yv, Scalar zv);
 
         // Physical properties
-        Scalar mass = 1;
-        Scalar friction = 0.5;
+        Scalar mass;
+        Scalar friction;
         std::optional<Scalar> restitution;
         std::optional<Scalar> margin;
 
         // Set to true to make the physics body take its position
         // from the irrilicht node.  Collisions from physics objects
         // won't affect it but it will affect other objects.
-        bool isKinematic = false;
+        bool isKinematic;
 
         // Set to true to cause the object never to deactivate.
         // This is important if the object might be moved by means
         // other than something colliding with it.  (Note: isKinematic also disables deactivation.)
-        bool disableDeactivation = false;
+        bool disableDeactivation;
 
         GravityType gravityType = GravityType::default_;
         std::optional<Scalar> gravityX;
         std::optional<Scalar> gravityY;
         std::optional<Scalar> gravityZ;
-        GameObjStyle& setGravity(Scalar x, Scalar y, Scalar z);
 
         // 1 gives no damping and 0 is fully damped.
-        Scalar linDamping = 0.0;
-        Scalar angDamping = 0.0;
-        GameObjStyle& setDamping(Scalar lin, Scalar ang);
+        Scalar linDamping;
+        Scalar angDamping;
 
         static constexpr ShortBitMask DefaultCollisionGroup = Bitmask(-1) ^ Constants::CameraFilter;
         static constexpr ShortBitMask DefaultCollisionMask = Constants::AllFilter;
 
         // Collision properties
-        ShortBitMask collisionGroup = DefaultCollisionGroup; //DefaultFilter
-        ShortBitmask collisionMask = DefaultCollisionMask;
-        CollisionResponseTags collisionResponseTag = CollisionResponseTags::none;
+        ShortBitMask collisionGroup;
+        ShortBitmask collisionMask;
+        CollisionResponseTags collisionResponseTag;
 
         // Dimensions - sphere/cylinder/cone
-        Scalar radius = 1;
-        Scalar length = 1;
-        bool closeEnds = true;  // for cylinder
+        Scalar radius;
+        Scalar length;
+        bool closeEnds;  // for cylinder
 
         // Dimensions - box
-        Scalar xSize = 1;
-        Scalar ySize = 1;
-        Scalar zSize = 1;
-
-        // Set xSize,ySize,zSize all at once
-        GameObjStyle& setSize(Scalar xSize, Scalar ySize, Scalar zSize);
-
-        // Physical shape
-        ObjShapes physShape = ObjShapes::box;
-
-        // Display shape
-        ObjShapes dispShape ::= ObjShapes::box;
-
-        // Set physical and display shape at once.
-        GameObjStyle& setShape(ObjShapes s);
+        Scalar xSize;
+        Scalar ySize;
+        Scalar zSize;
 
         // Appearance properties
-        SColor color = SColor(255, 255, 255, 255);
+        SColor color;
 
         // Note:  This method puts alpha last as opposed to SColor which has it first.
         GameObjStyle& setColor(IntColor r, IntColor g, IntColor b, IntColor a = 255);
@@ -176,7 +172,7 @@ namespace Glue {
         std::map<TextureKeys, ITexture*> textureMap;
 
         // Set to a file name to load the texture.
-        ITexture* texture = nullptr;
+        ITexture* texture;
         GameObjStyle& setTexture(ITexture* value);
 
         // Change the way TCoords wrap for e.g. cylinder or cone
@@ -189,47 +185,45 @@ namespace Glue {
         // Yaw, Roll, Pitch are used iff axisX==axisY==axisZ==0.
 
         // Rotation by axis and angle (better for first person view? might be useful for space ship game -?)
-        Scalar axisX = 0;
-        Scalar axisY = 0;
-        Scalar axisZ = 0;
-        Scalar angle = 0;
+        Scalar axisX;
+        Scalar axisY;
+        Scalar axisZ;
+        Scalar angle;
         GameObjStyle& setAxis(Scalar x, Scalar y, Scalar z);
 
         // Rotation by yaw, pitch, roll (better if your frame of reference is outside the object -?)
-        Scalar yaw = 0;
-        Scalar pitch = 0;
-        Scalar roll = 0;
+        Scalar yaw;
+        Scalar pitch;
+        Scalar roll;
 
         // Linear factor lets you constrain the object to 0 in X, Y or Z to keep the object on a 2D plane
         // 1 == normal, 0 == force 0
-        Scalar linFactorX = 1;
-        Scalar linFactorY = 1;
-        Scalar linFactorZ = 1;
+        Scalar linFactorX;
+        Scalar linFactorY;
+        Scalar linFactorZ;
         GameObjStyle& setLinearFactor(Scalar x, Scalar y, Scalar z);
 
         // Angular factor lets you disable certain rotations of the object (useful for 2D game)
-        Scalar angFactorX = 1;
-        Scalar angFactorY = 1;
-        Scalar angFactorZ = 1;
+        Scalar angFactorX;
+        Scalar angFactorY;
+        Scalar angFactorZ;
         GameObjStyle& setAngularFactor(Scalar x, Scalar y, Scalar z);
 
         // Scale the bullet and irrlicht shapes
-        Scalar dispScaleX = 1;
-        Scalar dispScaleY = 1;
-        Scalar dispScaleZ = 1;
-        Scalar physScaleX = 1;
-        Scalar physScaleY = 1;
-        Scalar physScaleZ = 1;
+        Scalar dispScaleX;
+        Scalar dispScaleY;
+        Scalar dispScaleZ;
+        Scalar physScaleX;
+        Scalar physScaleY;
+        Scalar physScaleZ;
         GameObjStyle& setDispScale(Scalar x, Scalar y, Scalar z);
         GameObjStyle& setPhysScale(Scalar x, Scalar y, Scalar z);
         GameObjStyle& setScale(Scalar x, Scalar y, Scalar z);
 
-        GameObjTypes gameObjType = GameObjTypes::rigid;
-
-        bool wireframe = false;
+        bool wireframe;
 
         // Default is an object is draggable iff it's not static
-        bool isMouseDraggable() const;
+        virtual bool isMouseDraggable() const;
 
         // If set to an Irrlicht mesh, display shape will use this mesh instead of the dispshape name.
         // (And if physShape name is "mesh", then physShape will be built from this mesh object.)
@@ -237,6 +231,6 @@ namespace Glue {
         // this field breaks the property of GameObjStyles that they describe 3D objects rather
         // than contain pieces of them (and that propery is desirable e.g. for things like
         // serialization of game objects).
-        IMesh mesh = nullptr;
+        IMesh mesh;
     };
 }
