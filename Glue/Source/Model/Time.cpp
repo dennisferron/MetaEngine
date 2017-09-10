b86b2cdf01a14b1ea7a276aae1af0b9550b4f0a3
Dennis Ferron <system.windows.codeslinger@gmail.com>
2017-09-10 16:40:17 -0500

    Converted Time.cpp.

--- Glue/Source/Model/Time.cpp
+++ Glue/Source/Model/Time.cpp
@@ -56,133 +56,59 @@ void Time::_processTimeoutEvents()
     }
 }
 
-Time& Time::setDeviceTimer(ITimer* value);
-void Time::setTimeout(RelTime delay, std::function<RelTime(RelTime)> action);
-RelTime Time::elapsed() const;
-void Time::runLoop();
-void Time::doEvents(EventWhen when, EventWhat what);
-
+Time& Time::setDeviceTimer(ITimer* value)
+{
+    deviceTimer = value;
 }
 
+void Time::setTimeout(RelTime delay, std::function<RelTime(RelTime)> action)
+{
+    AbsTime fromTime = currentTime;
+    AbsTime atTime = fromTime + delay;
+    TimeoutEvent evt({fromTime, atTime, action});
+    timeout_events.insert(evt);
+}
 
-method(
+RelTime Time::elapsed() const
+{
+    retur currentTime - lastTime;
+}
 
-Time := Object clone lexicalDo(
+void Time::runLoop()
+{
+    while(shouldRun)
+    {
+        lastTime = currentTime;
+        currentTime = deviceTimer->getTime();
 
-	events ::= nil
-	deviceTimer ::= nil
-	currentTime := 0
-	lastTime := 0
+        doEvents(EventWhen::before, EventWhat::frame);
 
-    shouldRun ::= nil
+        _processTimeoutEvents();
 
-    beforeFrame ::= nil
-    beforePhysics ::= nil
-    onPhysics ::= nil
-    afterPhysics ::= nil
-    beforeGraphics ::= nil
-    onGraphics ::= nil
-    afterGraphics ::= nil
-    afterFrame ::= nil
+        doEvents(EventWhen::before, EventWhat::physics);
+        doEvents(EventWhen::on, EventWhat::physics);
+        doEvents(EventWhen::after, EventWhat::physics);
 
-	init := method(
-        setEvents(List clone)
+        doEvents(EventWhen::on, EventWhat::frame);
 
-        setBeforeFrame(List clone)
-        setBeforePhysics(List clone)
-        setOnPhysics(List clone)
-        setAfterPhysics(List clone)
-        setBeforeGraphics(List clone)
-        setOnGraphics(List clone)
-        setAfterGraphics(List clone)
-        setAfterFrame(List clone)
-    )
+        doEvents(EventWhen::before, EventWhat::graphics);
+        doEvents(EventWhen::on, EventWhat::graphics);
+        doEvents(EventWhen::after, EventWhat::graphics);
 
-    setDeviceTimer := method(value,
-        deviceTimer = value
-        currentTime = deviceTimer call
-        lastTime = currentTime
-        self
-    )
+        doEvents(EventWhen::after, EventWhat::frame);
+    }
+}
 
-	Event := Object clone do(
-		atTime ::= nil
-		action ::= nil
-	)
+void Time::doEvents(EventWhen when, EventWhat what) const
+{
+    for (auto const& ee : engine_events)
+        if (ee.when == when && ee.what == what)
+            ee.action(TimeInfo {currentTime - lastTime, currentTime, lastTime});
+}
 
-	_processEvents := method(
-		tempList := events clone
-		tempList foreach(e,
-			if(e atTime <= currentTime,
-				nextTime := e action call
-				if(nextTime > 0, setTimeout(nextTime, e action))
-				events remove(e)
-			)
-		)
+void Time::add_handler(EventWhen when, EventWhat what, std::function<void(TimeInfo)> action)
+{
+    engine_events.push_back(EngineEvent {when, what, action});
+}
 
-		/*
-		while(events first != nil and events first atTime <= currentTime,
-			e := events removeFirst
-			nextTime := e action call
-			if(nextTime > 0, setTimeout(nextTime, e action))
-		)
-		*/
-	)
-
-	_getEventPos := method(atTime,
-		// Linear search; change to better algorithm if adding events gets slow.
-		events foreach(i, v,
-			if(v atTime >= atTime,
-				return i
-			)
-		)
-		return 0
-	)
-
-	setTimeout := method(delay, action,
-		atTime := currentTime + delay
-        events insertAt(
-            Event clone setAtTime(atTime) setAction(action),
-            _getEventPos(atTime)
-        )
-	)
-
-	elapsed := method(currentTime - lastTime)
-
-    runLoop := method(
-
-        if( deviceTimer == nil,
-            Exception raise("No deviceTimer set up in Time component.")
-        )
-
-        //Profiler profile(
-            while(shouldRun call,
-
-                lastTime = currentTime
-                currentTime = deviceTimer call
-
-                doEvent(beforeFrame)
-
-                _processEvents
-
-                doEvent(beforePhysics)
-                doEvent(onPhysics)
-                doEvent(afterPhysics)
-
-                doEvent(beforeGraphics)
-                doEvent(onGraphics)
-                doEvent(afterGraphics)
-
-                doEvent(afterFrame)
-            )
-        //)  // end Profiler profile
-    )
-
-    doEvent := method(handlerList,
-        handlerList foreach(handler,
-            handler call(currentTime - lastTime, currentTime, lastTime)
-        )
-    )
-)
-
-)
+}
